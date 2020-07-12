d8eaff83b485498cc529fa78fafb7c3d7d1c01db
Cyborger1 <45152844+Cyborger1@users.noreply.github.com>
2020-07-12 13:47:26 -0400

    Modified internal spawn tracking logic to use WorldPoints instead. This helps with reloads (e.g. doing a random event) not messing up the spawn counter.
    The total spawn counter is now for individual events rather than actual mark counts. Helps with dropping large grace mark stacks destroying the spawns per hour.
    
    General fixes.

--- src/main/java/com/mogcounter/MOGSession.java
+++ src/main/java/com/mogcounter/MOGSession.java
@@ -1,36 +1,12 @@
-/*
- * Copyright (c) 2018, Seth <http://github.com/sethtroll>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package com.mogcounter;
 
-import javax.inject.Singleton;
 import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
-
+import net.runelite.api.coords.WorldPoint;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 class MOGSession
 {
@@ -44,25 +20,41 @@ class MOGSession
 	@Getter(AccessLevel.PACKAGE)
 	private Instant firstMarkSpawn;
 	@Getter(AccessLevel.PACKAGE)
-	private int totalMarksSpawned;
+	private int totalMarkSpawnEvents;
 	@Getter(AccessLevel.PACKAGE)
-	private int marksPerHour;
+	private int spawnsPerHour;
 
-	void incrementMarksOfGraceSpawned(int inc)
+	private final Map<WorldPoint, Integer> markPoints = new HashMap<>();
+	private boolean isDirty;
+
+	void addMarkTile(WorldPoint point, int markCount)
 	{
-		marksSpawned += inc;
-		if (marksSpawned < 0) marksSpawned = 0;
+		markPoints.put(point, markCount);
+		isDirty = true;
+	}
+
+	void removeMarkTile(WorldPoint point)
+	{
+		markPoints.remove(point);
+		isDirty = true;
 	}
 
 	void checkMarkSpawned()
 	{
+		if (!isDirty) return;
+		isDirty = false;
+
+		marksSpawned = 0;
+		for (int i : markPoints.values())
+			marksSpawned += i;
+
 		Instant now = Instant.now();
 		if(marksSpawned > lastMarksSpawned)
 		{
 			if (firstMarkSpawn == null)
 				firstMarkSpawn = now;
 			lastMarkSpawn = now;
-			totalMarksSpawned += (marksSpawned - lastMarksSpawned);
+			totalMarkSpawnEvents++;
 		}
 		lastMarksSpawned = marksSpawned;
 
@@ -71,12 +63,11 @@ class MOGSession
 			Duration timeSinceStart = Duration.between(firstMarkSpawn, now);
 			if (!timeSinceStart.isZero())
 			{
-				double val = (double) totalMarksSpawned * (double) HOUR.toMillis() / (double) timeSinceStart.toMillis();
-				if (val > 999) marksPerHour = 999;
-				else marksPerHour = (int) val;
+				double val = (double) totalMarkSpawnEvents * (double) HOUR.toMillis() / (double) timeSinceStart.toMillis();
+				if (val > 999) spawnsPerHour = 999;
+				else spawnsPerHour = (int) val;
 			}
 		}
-
 	}
 
 	void clearCounters()
@@ -85,13 +76,17 @@ class MOGSession
 		lastMarkSpawn = null;
 		firstMarkSpawn = null;
 		marksSpawned = 0;
-		totalMarksSpawned = 0;
-		marksPerHour = 0;
+		totalMarkSpawnEvents = 0;
+		spawnsPerHour = 0;
+		markPoints.clear();
+		isDirty = false;
 	}
 
 	void clearSpawnedMarks()
 	{
 		marksSpawned = 0;
 		lastMarksSpawned = 0;
+		markPoints.clear();
+		isDirty = false;
 	}
 }
